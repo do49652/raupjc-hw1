99d0872b9ed7e7f73bf0aeaf15d8ed1b6dafc64e
Dario Oroz <dario.oroz@fer.hr>
2017-10-14 17:25:46 +0200

    Zadatak 3

---
+++ Zadatak3/GenericListEnumerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Zadatak3
+{
+    public class GenericListEnumerator<T> : IEnumerator<T>
+    {
+        private int _currentIndex;
+        private readonly GenericList<T> genericList;
+
+        public GenericListEnumerator(GenericList<T> genericList)
+        {
+            _currentIndex = 0;
+            this.genericList = genericList;
+            Current = this.genericList.GetElement(_currentIndex);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            if (_currentIndex + 1 >= genericList.Count)
+                return false;
+
+            Current = genericList.GetElement(++_currentIndex);
+            return true;
+        }
+
+        public void Reset()
+        {
+            _currentIndex = 0;
+            Current = genericList.GetElement(_currentIndex);
+        }
+
+        public T Current { get; private set; }
+
+        object IEnumerator.Current => Current;
+    }
+}
---
+++ Zadatak3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Zadatak3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stringList = new GenericList<string>();
+
+            stringList.Add("1");
+            stringList.Add("asd");
+            stringList.Add("asdasdew");
+            stringList.Add("234234");
+            stringList.Add("1111111");
+            stringList.Add("1444");
+
+            // foreach
+            foreach (var value in stringList)
+                Console.WriteLine(value);
+            // foreach without the syntax sugar
+            var enumerator = stringList.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var value = enumerator.Current;
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
